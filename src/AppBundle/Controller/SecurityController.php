8e6ff0f4286c57feef5cf6dcaec1afd74a9ac749
Janis Boguss <janis.boguss@inbox.lv>
2016-05-10 21:19:24 +0300

    Security controller && User entity

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request)
+    {   
+         $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render(
+            'AppBundle:Security:login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
+    }
+
+    /**
+     * @Route("/login_check", name="login_check")
+     */
+    public function loginCheckAction()
+    {
+        return $this->render('AppBundle:Security:login_check.html.twig', array(
+            // ...
+        ));
+    }
+
+}
+//Available types: array, simple_array, json_array, object, 
+//boolean, integer, smallint, bigint, string, text, datetime, datetimetz, 
+//date, time, decimal, float, binary, blob, guid.
